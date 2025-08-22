9459ec8e38e535df1a150bc3950e1bbc52d4c414
yash1302 <jadhavyash623@gmail.com>
2025-08-22 08:06:04 +0530

    first commit

---
+++ routes/doctorRoute.js
@@ -0,0 +1,32 @@
+import express from "express";
+import {
+  loginDoctor,
+  appointmentsDoctor,
+  appointmentCancel,
+  doctorList,
+  changeAvailablity,
+  appointmentComplete,
+  doctorDashboard,
+  doctorProfile,
+  updateDoctorProfile,
+} from "../controllers/doctorController.js";
+import authDoctor from "../middleware/authDoctor.js";
+const doctorRouter = express.Router();
+
+doctorRouter.post("/login", async (req, res, next) => {
+  try {
+    await loginDoctor(req, res, next);
+  } catch (error) {
+    next(error);
+  }
+});
+doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel);
+doctorRouter.get("/appointments", authDoctor, appointmentsDoctor);
+doctorRouter.get("/list", doctorList);
+doctorRouter.post("/change-availability", authDoctor, changeAvailablity);
+doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete);
+doctorRouter.get("/dashboard", authDoctor, doctorDashboard);
+doctorRouter.get("/profile", authDoctor, doctorProfile);
+doctorRouter.post("/update-profile", authDoctor, updateDoctorProfile);
+
+export default doctorRouter;
