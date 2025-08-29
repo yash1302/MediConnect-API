3ad6c65025bb54956129e8aa04c42856a40c129f
yashvardhan jadhav <jadhavyash623@gmail.com>
2025-08-29 19:36:24 +0530

    PATCH : added socket connection

---
+++ app/models/chatModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema({
+  senderId: { type: String, required: true },
+  receiverId: { type: String, required: true },
+  message: { type: String, required: true },
+  date: { type: Number, required: true },
+});
+
+const chatModel = mongoose.models.chat || mongoose.model("chat", chatSchema);
+export default chatModel;
